03cc3c4608ca2d2812814616bb9967c99901a15d
Scott <scottb7089@gmail.com>
2016-10-18 15:34:14 -0400

    Added remaining templates and new corresponding models

---
+++ HealthPlanSurveyService/Models/SurveyActivePlan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UBA.Modules.HealthPlanSurveyService.Models
+{
+    public class SurveyActivePlan
+    {
+        public SurveyResponse_ActivePlan ActivePlan { get; set; }
+        public SurveyRxPlan RxPlan { get; set; }
+    }
+}
---
+++ HealthPlanSurveyService/Models/SurveyRxPlan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UBA.Modules.HealthPlanSurveyService.Models
+{
+    public class SurveyRxPlan
+    {
+        public SurveyResponse_RxPlan RxPlan { get; set; }
+        public List<SurveyResponse_RxPlanTier> PlanTiers { get; set; }
+    }
+}
