bf5ebe5d0caadf15238608e67a5a89e5234a03d2
Scott <scottb7089@gmail.com>
2016-10-27 13:24:07 -0400

    Changed New Survey DIV from modal to toggled display

---
+++ HealthPlanSurvey/controllers/SurveyStartController.js
@@ -0,0 +1,24 @@
+﻿angular.module('SurveyWrangler').controller('SurveyStartController', function ($scope, $uibModalInstance) {
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+    $scope.startNav = function (startType, id) {
+        if (startType == 0) {
+            // start a new survey
+            $location.path("/surveys/new").then(function(ref){
+                $uibModalInstance.close(ref);
+            });
+
+        } else {
+
+            // carry forward survey data
+            $location.path("/surveys/" + id + "/carryforward/").then(function(ref){
+                $uibModalInstance.close(ref);
+            });
+
+        }
+    };
+
+});
